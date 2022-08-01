83927bb9f4d641edadaf871e56865f9bb9c2171a
Derekma666 <44194994+derekma666@users.noreply.github.com>
2022-08-01 21:08:07 +0100

    update Stack.java

--- Algorithms/Readme.md
+++ Algorithms/Readme.md
@@ -3,7 +3,8 @@
 ## Contents
 ### Part 1: Pseudo-code, algorithm analysis and linear data structures
 * [Pseudo-code]()
-* Stack, Queues
+* Stack
+* Queues
 * Linked list
 
 ### Part 2: Sorting algorithms
@@ -20,4 +21,48 @@
 ### Part 4: Priority Queues, Huffman trees, Graphs
 * Priority Queues, Huffman trees
 * Graphs and graph representations
+
+**Algorithm basics**
+- pseudo-code conventions
+- analysing algorithms using RAM
+- worst case analysis
+- best case analysis
+- express the time complexity using asymptotic
+- notations
+- solving recurrences via recursion tree
+- ALL sorting algorithms covered in this module.
+
+**Stack**:
+ - LIFO
+ - S.top
+ - basic operations (O(1))
+- PUSH(S, x)
+- POP(S)
+- STACK-EMPTY(S)
+
+```java
+// STACK-EMPTY(S)
+if S.top == 0
+    return TRUE
+else 
+    return FALSE
+```
+
+```java
+// PUSH(S, x)
+S.top = S.top + 1
+S[S.top] = x
+```
+
+```java
+// POP(S)
+if STACK-EMPTY(S)
+    error "underflow"
+else S.top = S.top - 1
+    return S[S.top + 1]
+```
+
+**Queue**:
+Q.head is the first element of queue.
+Q.tail is the next insersion position of the last element. 
 
--- Algorithms/src/Stack.java
+++ Algorithms/src/Stack.java
@@ -1,12 +1,64 @@
-public class Stack {
+import java.util.Scanner;
 
+public class Stack {
     // First in Last out (FILO)
+    public static void main(String[] args) {
+        // test the ArrayStack
+        // create an ArrayStack object -> stack
+        ArrayStack stack = new ArrayStack(4);
+        // create an empty string
+        String key = "";
+        // control exit the manu or not
+        boolean loop = true;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(loop) {
+            System.out.println("show: show the stack");
+            System.out.println("exit: exit the stack");
+            System.out.println("push: push a data into stack");
+            System.out.println("pop: pop a data from the stack");
+
+            System.out.println("Please input your option: ");
+
+            key = scanner.next();
+            switch (key) {
+                case "show":
+                    stack.list();
+                    break;
+
+                case "push":
+                    System.out.println("Please enter a value:");
+                    int value = scanner.nextInt();
+                    stack.push(value);
+                    break;
+
+                case "pop":
+                    try {
+                        int res = stack.pop();
+                        System.out.printf("pop the data: %d\n", res);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+
+                case "exit":
+                    scanner.close();
+                    loop = false;
+
+                default:
+                    break;
+            }
+        }
+        System.out.println("...........program exit.........");
+    }
 }
+
+// define a class to represent stack
 class ArrayStack {
     private int maxSize; //size of stack
-    private int[] stack; //arrays, for simulate the stack, data in arrays
-    private int top = -1; // top, initialisation: -1
-
+    private int[] stack; //arrays, for simulate the stack, data in the array
+    private int top = -1; // top, initialisation: -1, no data
 
     // constructor/methods
     public ArrayStack(int maxSize) {
@@ -25,6 +77,41 @@ class ArrayStack {
     }
 
     // push
+    public void push(int value){
+        // step 1: check the stack is full or not?
+        if(isFull()){
+            System.out.println("Full");
+            return;
+        }
+        // step 2: top + 1, and assign the value to the top position
+        top++;
+        stack[top] = value;
+    }
+
+    // pop: return the data of the top position
+    public int pop() {
+        // step 1: check the stack is empty or not?
+        if(isEmpty()){
+            //give
+            throw new RuntimeException("** Empty, no data **");
+        }
+        int value = stack[top];
+        top--;
+        return value;
+    }
+
+    //traverse, need start from the stack top
+    public void list(){
+        // step 1: check the stack is empty or not?
+        if (isEmpty()){
+            System.out.println("Empty stack, no data");
+            return;
+        }
+        // display data
+        for (int i = top; i >= 0; i--){
+            System.out.printf("stack[%d] = %d\n", i, stack[i]);
+        }
+    }
 
 
 }
